b8b5be634c56c5f4a430262d05ca2ea592584d63
Krishan Dwivedi <krishandwive9@gmail.com>
2024-06-08 21:09:58 +0530

    added shacn and darkmode support

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import { ThemeToggle } from "./components/Themetoggle";
 
 export default function Home() {
-  return <h1>Hello from the index page!</h1>;
+  return (
+    <div>
+      <h1>Hello from the index page!</h1>
+      <ThemeToggle />
+    </div>
+  );
 }
