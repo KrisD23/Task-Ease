6584f5899096d0dd600308db6a690eaaba0ae76d
Krishan Dwivedi <krishandwive9@gmail.com>
2024-06-09 19:21:22 +0530

    stripe integration fully works

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -132,10 +132,11 @@ export default async function BillingPage() {
           </div>
 
           <div className="mt-4 flex items-baseline text-6xl font-extrabold">
-            $30 <span className="ml-1 text-2xl text-muted-foreground">/mo</span>
+            $9.99{" "}
+            <span className="ml-1 text-2xl text-muted-foreground">/mo</span>
           </div>
           <p className="mt-5 text-lg text-muted-foreground">
-            Write as many notes as you want for $30 a Month
+            Write as many notes as you want for $9.99 a Month
           </p>
         </CardContent>
         <div className="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-secondary rounded-lg m-1 space-y-6 sm:p-10 sm:pt-6">
